0af031db1498f48fc8dbc8dc536f3889d799c4c1
2oothpick <kobbyboateng14@gmail.com>
2024-03-14 06:27:11 +0000

    Fix gettr and setter methods

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,43 +1,48 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw TypeError('name must be a String');
-    if (typeof length !== 'number') throw TypeError('length must be a Number');
-    if (!Array.isArray(students)) throw TypeError('students must be an Array');
-    for (const el of students) {
-      if (typeof el !== 'string') { throw TypeError('students must contain strings'); }
-    }
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    /*     if (typeof (name) !== 'string') { throw TypeError('Name must be a string'); }
+    if (typeof (length) !== 'number') { throw TypeError('Length must be a number'); }
+    if (!Array.isArray(students)) { throw TypeError('students must be an Array'); } else {
+      students.forEach((item) => { if (typeof item !== 'string') {
+         throw TypeError('students must contain string'); } return true; });
+    } */
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  set name(name) {
-    if (typeof name !== 'string') throw TypeError('name must be a String');
-    this._name = name;
-  }
-
+  // getter for name
   get name() {
     return this._name;
   }
 
-  set length(length) {
-    if (typeof length !== 'number') throw TypeError('length must be a Number');
-    this._length = length;
+  // setter for name
+  set name(value) {
+    if (typeof (value) !== 'string') { throw TypeError('Name must be a string'); }
+    this._name = value;
   }
 
+  // getter for length
   get length() {
     return this._length;
   }
 
-  set students(students) {
-    if (!Array.isArray(students)) throw TypeError('students must be an Array');
-    for (const el of students) {
-      if (typeof el !== 'string') { throw TypeError('students must contain strings'); }
-    }
-    this._students = students;
+  // setter for length
+  set length(value) {
+    if (typeof (value) !== 'number') { throw TypeError('Length must be a number'); }
+    this._length = value;
   }
 
+  // getter for students
   get students() {
     return this._students;
+  }
+
+  // setter for students
+  set students(value) {
+    if (!Array.isArray(value)) { throw TypeError('students must be an Array'); } else {
+      value.forEach((item) => { if (typeof item !== 'string') { throw TypeError('students must contain string'); } return true; });
+    }
+    this._students = value;
   }
 }
