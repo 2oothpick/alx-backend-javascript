371e6fdfdbd61fe39a7d652413a39bfe40edd83e
2oothpick <kobbyboateng14@gmail.com>
2024-04-29 20:21:10 +0000

    Add function displayMessage

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/* 
+Module contains the function
+displayMessage that prints
+its string argument to STDOUT
+*/
+
+function displayMessage(message) {
+    console.log(message);
+}
+  
+module.exports = displayMessage;
