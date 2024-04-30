d62bbfe9ec4b390986ae6e7cf98607ccd3711747
2oothpick <kobbyboateng14@gmail.com>
2024-04-30 04:37:57 +0000

    Add 1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+console.log('Welcome to Holberton School, What is your name?');
+// take input from stdin
+const { stdin } = process;
+stdin.on('data', (name) => {
+  // print user input to console.
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+    // process.exit()
+  }
+});
+stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
