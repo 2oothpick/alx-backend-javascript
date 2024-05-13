c4c4ea640fabacd52222455b32cf866e1efdad53
2oothpick <kobbyboateng14@gmail.com>
2024-05-13 21:56:00 +0000

    Refactor function Refactored to make funciton readable

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,12 +2,7 @@ export default function createReportObject(employeesList) {
   const send = {
     allEmployees: { ...employeesList },
     getNumberOfDepartments(employeesList) {
-      let arr = 0;
-      // eslint-disable-next-line no-unused-vars
-      for (const i of Object.keys(employeesList)) {
-        arr += 1;
-      }
-      return arr;
+      return Object.keys(employeesList).length;
     },
   };
   return send;
