4f5e5730d14faf251f485909d6c1edb378763155
2oothpick <kobbyboateng14@gmail.com>
2024-06-26 23:48:33 +0000

    Add Teacer interface for task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,6 +1,11 @@
-class Person{
-    firstName: string
-    constructor(firstName: string){
-        this.firstName = firstName
-    }
+/* task 1 */
+
+interface Teacher {
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
+    location: string,
+    [key: string]: any
 }
+
