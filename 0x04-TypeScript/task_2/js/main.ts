becb05f605df4e9c777b1a9fc30bdd65977dc63d
2oothpick <kobbyboateng14@gmail.com>
2024-06-27 16:45:09 +0000

    Add string literal types for task_2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,3 +57,12 @@ function executeWork(employee: dirTeach){
         return employee.workTeacherTasks()
     }
 }
+type Subjects = 'Math' | 'History'
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math'
+    } else {
+        return 'Teaching History'
+    }
+}
