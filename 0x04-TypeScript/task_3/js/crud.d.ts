8667f8d22f1322ae0e4010f3940572dcfb02f309
2oothpick <kobbyboateng14@gmail.com>
2024-06-28 12:38:02 +0000

    Create ambient module for JS libraty

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,5 @@
+import {RowID, RowElement} from './interface'
+
+export function insertRow(RowElement): number
+export function deleteRow(RowID): void
+export function updateRow(RowID, RowElement): RowID
